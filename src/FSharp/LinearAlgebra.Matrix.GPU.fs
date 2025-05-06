d52b356f1a380875ce12b4a4b8c8cb320c947c10
catharsian <sage>
2025-05-06 19:28:57 +0300

    Implemented map2
    It works but it's messy

--- src/FSharp/LinearAlgebra.Matrix.GPU.fs
+++ src/FSharp/LinearAlgebra.Matrix.GPU.fs
@@ -32,7 +32,7 @@ namespace MathNet.Numerics.LinearAlgebra.GPU
 open System
 open MathNet.Numerics
 open MathNet.Numerics.LinearAlgebra
-
+open Brahma.FSharp
 
 /// A module which implements functional matrix operations.
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
@@ -42,7 +42,58 @@ module Matrix =
     let iter f (v: #Matrix<_>) = v
 
     /// !!!
-    let map2 f (v: #Matrix<_>) (w: #Matrix<_>) = v
+    let map2 (f: Quotations.Expr<'a -> 'b -> 'c>) (v: #Matrix<'a>) (w: #Matrix<'b>) =
+        let device = ClDevice.GetFirstAppropriateDevice() // ?
+        let context = RuntimeContext(device).ClContext
+
+        let n = v.RowCount
+        let m = v.ColumnCount
+        
+        let m1_gpu = context.CreateClArray<_>(v.ToColumnMajorArray(), HostAccessMode.NotAccessible)
+        let m2_gpu = context.CreateClArray<_>(w.ToColumnMajorArray(), HostAccessMode.NotAccessible)
+        let m3_gpu =
+            context.CreateClArray(
+                n * m,
+                HostAccessMode.NotAccessible,
+                deviceAccessMode=DeviceAccessMode.WriteOnly,
+                allocationMode = AllocationMode.Default
+            )
+
+        let localWorkSize = 4
+
+        let kernel =
+            <@
+                fun (r: Range2D) (m1: ClArray<_>) (m2: ClArray<_>) (m3: ClArray<_>)  ->
+                    let i = r.GlobalID0
+                    let j = r.GlobalID1
+
+                    m3.[i * m + j] <- (%f) m1[i * m + j] m2[i * m + j]
+            @>
+
+        let kernel = context.Compile kernel
+
+        let ndRange =
+            Range2D(
+                n,
+                m,
+                localWorkSize,
+                localWorkSize
+            )
+
+        let commandQueue = context.QueueProvider.CreateQueue()
+        let kernel = kernel.GetKernel()
+
+        commandQueue.Post(Msg.MsgSetArguments(fun () -> kernel.KernelFunc ndRange m1_gpu m2_gpu m3_gpu))
+        commandQueue.Post(Msg.CreateRunMsg<_, _> kernel)
+        let result : 'c[] = Array.zeroCreate(n * m)
+        let result = commandQueue.PostAndReply(fun ch -> Msg.CreateToHostMsg(m3_gpu, result, ch))
+        commandQueue.Post(Msg.CreateFreeMsg m1_gpu)
+            
+        commandQueue.Post(Msg.CreateFreeMsg m2_gpu)
+            
+        commandQueue.Post(Msg.CreateFreeMsg m3_gpu)
+
+        result |> DenseMatrix.raw n m
 
     /// !!!
     let mXm (v: #Matrix<_>) (w: #Matrix<_>) = v
